a5eed043dc93435a50fb6dd9cee6a88d9b9cad0c
sambutracy <tracysambut@gmail.com>
2024-08-17 14:41:37 +0300

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -1,49 +1,86 @@
 #!/usr/bin/python3
+""" This script converts markdown to HTML.
 """
-markdown2html.py: A script to convert Markdown files to HTML.
-"""
-
 import sys
-import os
+import re
+import hashlib
 
-def markdown_to_html(markdown_file, output_file):
+
+def convert(markdownFile, htmlFile):
+    """ Converts markdown to HTML.
     """
-    Converts a Markdown file to HTML and writes it to the output file.
-    
-    Args:
-        markdown_file (str): Path to the Markdown file.
-        output_file (str): Path to the output HTML file.
-    """
-    with open(markdown_file, 'r') as md_file:
-        lines = md_file.readlines()
+    try:
+        with open(markdownFile, 'r') as r:
+            with open(htmlFile, 'w') as w:
+                text = r.read()
+                text = re.sub(
+                            r'\*\*(.*?)\*\*', r'<b>\1</b>', text
+                            )
+                text = re.sub(r'__(.*?)__', r'<em>\1</em>', text)
+                text = re.sub(r'\[\[(.*?)\]\]', lambda m: f'{hashlib.md5(m.group(1).encode()).hexdigest()}', text)
+                text = re.sub(r'\(\((.*?)\)\)', lambda m: f'{re.sub("c", "", m.group(1), flags=re.IGNORECASE)}', text)
+                textLines = text.splitlines()
+                i = 0
+                while i < len(textLines):
+                    line = textLines[i]
+                    if line == '':
+                        i += 1
+                        continue
+                    if line.startswith('###### '):
+                        w.write('<h6>{}</h6>\n'.format(line[7:]))
+                    elif line.startswith('##### '):
+                        w.write('<h5>{}</h5>\n'.format(line[6:]))
+                    elif line.startswith('#### '):
+                        w.write('<h4>{}</h4>\n'.format(line[5:]))
+                    elif line.startswith('### '):
+                        w.write('<h3>{}</h3>\n'.format(line[4:]))
+                    elif line.startswith('## '):
+                        w.write('<h2>{}</h2>\n'.format(line[3:]))
+                    elif line.startswith('# '):
+                        w.write('<h1>{}</h1>\n'.format(line[2:]))
+                    elif line.startswith('- '):
+                        listStr = ""
+                        for j in range(i, len(textLines)):
+                            if textLines[j].startswith('-'):
+                                listStr += "<li>{}</li>\n".format(
+                                    textLines[j][2:])
+                            else:
+                                break
+                        w.write("<ul>\n{}</ul>\n".format(listStr))
+                        i = j
+                    elif line.startswith('* '):
+                        listStr = ""
+                        for j in range(i, len(textLines)):
+                            if textLines[j].startswith('*'):
+                                listStr += "<li>{}</li>\n".format(
+                                    textLines[j][2:])
+                            else:
+                                break
+                        w.write("<ol>\n{}</ol>\n".format(listStr))
+                        i = j
+                    else:
+                        listStr = ""
+                        for j in range(i, len(textLines)):
+                            if j+1 < len(textLines) and textLines[j + 1] == '':
+                                listStr += "{}".format(textLines[j])
+                                break
+                            elif j+1 >= len(textLines):
+                                listStr += "{}".format(textLines[j])
+                                break
+                            else:
+                                listStr += "{}\n<br/>\n".format(textLines[j])
+                        w.write("<p>\n{}\n</p>\n".format(listStr))
+                        i = j
+                    i += 1
+    except Exception as e:
+        print("Missing {}".format(markdownFile), file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
 
-    html_content = ""
-
-    for line in lines:
-        line = line.strip()
-        
-        # Check for headers
-        if line.startswith("#"):
-            header_level = line.count("#", 0, 6)  # count number of # at the start of the line
-            header_text = line[header_level:].strip()  # remove the heading hashes and strip any leading spaces
-            html_content += f"<h{header_level}>{header_text}</h{header_level}>\n"
-        else:
-            html_content += f"<p>{line}</p>\n"
-    
-    with open(output_file, 'w') as html_file:
-        html_file.write(html_content)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
-
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    if not os.path.isfile(markdown_file):
-        sys.stderr.write(f"Missing {markdown_file}\n")
-        sys.exit(1)
-
-    markdown_to_html(markdown_file, output_file)
-    sys.exit(0)
+    convert(sys.argv[1], sys.argv[2])
